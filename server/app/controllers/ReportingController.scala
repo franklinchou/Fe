8620039e2aabe51c3758ef57971c95e9be4532b2
Franklin Chou <franklin@peeriq.com>
2018-08-18 12:35:44 -0400

    added reporting endpoint

---
+++ server/app/controllers/ReportingController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import com.google.inject.Inject
+import play.api.mvc._
+
+class ReportingController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok()
+  }
+
+}
