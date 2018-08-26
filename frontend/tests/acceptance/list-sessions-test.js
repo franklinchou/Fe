2f6972153ce8d36b6fa102cf4f28d1b86d37d7f4
fmc <franklin@peeriq.com>
2018-08-25 23:26:55 -0400

    [feature] Initial cut of front end (#18)
    * added frontend
    
    * added routes
    
    * some tests
    
    * added about link test

---
+++ frontend/tests/acceptance/list-sessions-test.js
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list sessions', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('should show my sessions as home', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/sessions', 'should redirect to /sessions');
+  });
+
+  test('test about link', async function(assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to /about')
+  });
+
+});
