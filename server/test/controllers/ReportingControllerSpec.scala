a87f62f51205c893c11249da4f4be3e9bc83330a
Franklin Chou <franklin@peeriq.com>
2018-09-03 21:42:22 -0400

    fix reporting controller spec

--- server/test/controllers/ReportingControllerSpec.scala
+++ server/test/controllers/ReportingControllerSpec.scala
@@ -1,12 +1,25 @@
 package controllers
 
+import akka.stream.ActorMaterializer
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.test.Helpers._
 import play.api.test._
 import controllers.Headers._
+import lib.containers.StringContainer
+import models.{AbstractUserId, ExerciseSessionModel}
+import org.scalatest.mockito.MockitoSugar
+import services.ReportingService
+import org.mockito.Mockito.when
+import play.api.libs.json.Json
+import play.api.mvc.AnyContent
 
-class ReportingControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+import scala.concurrent.Future
+
+class ReportingControllerSpec
+  extends PlaySpec with MockitoSugar with GuiceOneAppPerTest with Injecting {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   "A Reporting Controller" should {
 
@@ -23,8 +36,25 @@ class ReportingControllerSpec extends PlaySpec with GuiceOneAppPerTest with Inje
 
     // Testing with proper user header
     s"$OK if the proper user header is given" in {
-      val request = FakeRequest(GET, "/logging", TEST_USER, "")
-      val method = route(app, request).get
+
+      /**
+        * For some reason using the FakeRequest.apply(method, endpoint, headers, body)
+        * creates object with signature FakeRequest[String] & compile time error:
+        *
+        * "could not find implicit value for parameter mat: akka.stream.Materializer"
+        */
+      val request =
+        FakeRequest(GET, "/logging")
+          .withHeaders(TEST_USER)
+
+      val rs = mock[ReportingService]
+      val mockReportingController = new ReportingController(stubControllerComponents(), rs)
+
+      val user = TEST_USER.get("user").getOrElse("")
+      val u = StringContainer.apply[AbstractUserId](user)
+      when(rs.findAll(u)).thenReturn(Future {List.empty[ExerciseSessionModel]} )
+
+      val method = mockReportingController.index()(request)
       assert(status(method) == OK)
     }
 
