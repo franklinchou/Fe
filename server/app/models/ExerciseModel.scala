cc6a8d4c7fd1aeebfbc89771bf94309f1a5f0150
Franklin Chou <franklin@peeriq.com>
2018-10-17 11:32:09 -0400

    exercise resource marhsalling

--- server/app/models/ExerciseModel.scala
+++ server/app/models/ExerciseModel.scala
@@ -1,5 +1,7 @@
 package models
 
+import java.util.UUID
+
 import ai.x.play.json.Jsonx
 import lib.containers.StringContainer
 import models.exercises._
@@ -12,11 +14,44 @@ object ExerciseModel {
     */
   implicit lazy val jsFormat: OFormat[ExerciseModel] = Jsonx.formatCaseClass[ExerciseModel]
 
+
+  /**
+    * Create an exercise model.
+    *
+    * Note that in a /POST method, the call should not include an id. The backend will
+    * generate an id and return the id (via jsonapi) in the response.
+    *
+    * @param exercise
+    * @param description
+    * @param variation
+    * @param weight
+    * @return
+    */
+  def apply(exercise: Exercise,
+            description: String,
+            variation: String,
+            weight: BigDecimal): ExerciseModel = {
+
+    val uuid = UUID.randomUUID().toString
+    val id = StringContainer[AbstractModelId](uuid)
+
+    ExerciseModel(
+      id = id,
+      exercise = exercise,
+      description = description,
+      variation = variation,
+      weight = weight
+    )
+
+  }
+
+
+
 }
 
 case class ExerciseModel(id: StringContainer[AbstractModelId],
                          exercise: Exercise,
                          description: String,
                          variation: String,
-                         weight: Double
+                         weight: BigDecimal
                          /* duration: Option[???] */) extends AbstractModel
