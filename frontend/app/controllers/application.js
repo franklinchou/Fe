b04237221f5c7ebf519cd93a6a885f2a230f28ce
Franklin Chou <franklin@peeriq.com>
2018-11-03 10:08:57 -0400

    conditional display working

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -1,8 +1,16 @@
 import Controller from '@ember/controller';
 import ENV from 'fe/config/environment';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
 
     environment: ENV.environment,
+
+    displayInfoHeader: computed('environment', function() {
+        if (this.get('environment') === 'production') {
+            return false;
+        }
+        return true;
+    })
 
 });
