24a05007446118e7d6d9b4ae206181f3ea667ea3
Franklin Chou <franklin@peeriq.com>
2018-11-03 17:25:16 -0400

    saving

--- frontend/app/components/panel-all-sessions-overview.js
+++
@@ -1,6 +0,0 @@
-import Component from '@ember/component';
-
-// components/panel-all-sessions-overview.js
-export default Component.extend({
-
-});
--- frontend/app/routes/sessions.js
+++ frontend/app/routes/sessions.js
@@ -1,20 +1,14 @@
 import Route from '@ember/routing/route';
-import { inject } from '@ember/service';
-
+    
 import RSVP from 'rsvp';
 
 // routes/sessions.js
 export default Route.extend({
 
-    store: inject(),
-
     model(/* empty for now */) {
-        const store = this.get('store');
 
         // Send a call to GET /sessions
-        const sessions = store.findAll('session');
-
-        console.log(sessions);
+        const sessions = this.store.findAll('session');
         
         return RSVP.hash({
             sessions
